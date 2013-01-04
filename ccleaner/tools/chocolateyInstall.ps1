b7ae6be41057699a1f13939a0c15131e443e49bd
Erwann <eplouhinec-mail@yahoo.fr>
2013-01-05 00:33:35 +0100

    First package Ccleaner in version 3.26.1888

---
+++ ccleaner/tools/chocolateyInstall.ps1
@@ -0,0 +1,8 @@
+﻿$packageName = 'ccleaner'
+$installerType = 'EXE'
+$url = 'http://fs30.filehippo.com/1293/f19c61d6981e4c3ab4771ec95dd5abd5/ccsetup326.exe'
+$url64 = $url
+$silentArgs = '/S'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"  -validExitCodes $validExitCodes
